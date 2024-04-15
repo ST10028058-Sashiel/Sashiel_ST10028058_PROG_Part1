0c6304f4a2433544a4c0db38b7d452cd53c0c3b7
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-04-15 21:12:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sashiel_PROG_Part1
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+
+    class RecipeStep
+    {
+        public string Description { get; set; }
+    }
+
+    class Recipe
+    {
+        public List<Ingredient> Ingredients { get; set; }
+        public List<RecipeStep> Steps { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+            Steps = new List<RecipeStep>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
+        
+   
