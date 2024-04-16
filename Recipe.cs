e3f1be43f4b57ce5a7757d1abc73c4363542ad08
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-04-16 11:51:16 +0200

    Created methods in the Recipe Class.

--- Recipe.cs
+++ Recipe.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Sashiel_PROG_Part1
 {
-    class Recipe
+    public class Ingredient
     {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public double InitialQuantity { get; set; } // Store the initial quantity
+        public double Quantity { get; set; } // Quantity for scaling
+    }
+
+    public class Step
+    {
+        public string Description { get; set; }
+    }
+
+    public class Recipe
+    {
+
+
     }
 }
