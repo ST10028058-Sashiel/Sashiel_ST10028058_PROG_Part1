6665dda9e8d19f0a8bde4de3c76752ab9fd20082
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-04-16 11:58:06 +0200

    Added Arrays to store the recipe steps

--- Recipe.cs
+++ Recipe.cs
@@ -21,7 +21,66 @@ namespace Sashiel_PROG_Part1
 
     public class Recipe
     {
+        private Ingredient[] ingredients;
+        private Step[] steps;
 
+        public void SetIngredients(Ingredient[] ingredients)
+        {
+            this.ingredients = ingredients;
+            foreach (var ingredient in this.ingredients)
+            {
+                // Store the initial quantity
+                ingredient.InitialQuantity = ingredient.Quantity;
+            }
+        }
+
+        public void SetSteps(Step[] steps)
+        {
+            this.steps = steps;
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in ingredients)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{ingredient.Quantity} {ingredient.Unit} of {ingredient.Name}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("\nInstructions:");
+            foreach (var step in steps)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(step.Description);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void ScaleRecipe(double scale)
+        {
+            foreach (var ingredient in ingredients)
+            {
+                // Scale the quantity based on the initial value
+                ingredient.Quantity = ingredient.InitialQuantity * scale;
+            }
+        }
+
+        public void ReturnToBase()
+        {
+            foreach (var ingredient in ingredients)
+            {
+                // Set the quantity back to the initial value
+                ingredient.Quantity = ingredient.InitialQuantity;
+            }
+        }
+
+        public void ClearRecipe()
+        {
+            ingredients = null;
+            steps = null;
+        }
 
     }
 }
